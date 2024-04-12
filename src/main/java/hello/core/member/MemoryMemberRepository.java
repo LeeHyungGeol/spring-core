17934d672cadf27efeec11a8d5b87da4e363d703
LeeHyungGeol <rjf1138@naver.com>
2024-04-12 18:15:00 +0900

    feat(스프링-핵심-원리-이해2---객체-지향-원리-적용): 관심사의 분리

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -7,6 +7,10 @@ public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
 
+
+    public MemoryMemberRepository() {
+    }
+
     @Override
     public void save(Member member) {
         store.put(member.getId(), member);
