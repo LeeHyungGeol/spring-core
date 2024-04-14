cb2686d43555c68bc160f1ccbe3f8a65e657d0f6
LeeHyungGeol <rjf1138@naver.com>
2024-04-14 14:49:59 +0900

    feat(컴포넌트-스캔): 탐색 위치와 기본 스캔 대상

---
+++ src/test/java/hello/core/AutoAppConfigTest.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class AutoAppConfigTest {
+
+    @Test
+    void basicScan() throws Exception {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean("memberServiceImpl", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+}
