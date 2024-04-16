30752ab7db9e372809f521dd18cbb9c5c5651c3a
LeeHyungGeol <rjf1138@naver.com>
2024-04-16 15:41:02 +0900

    feat(빈-생명주기-콜백): 인터페이스 InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -1,6 +1,8 @@
 package hello.core.lifecycle;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
